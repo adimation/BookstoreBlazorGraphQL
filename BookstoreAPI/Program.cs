5dc6d8249a00d8ba99c1020c2f9bd0acaf47e93d
ADNANPC\adnan <adnan.ahmed316@gmail.com>
2024-06-30 02:40:37 +0500

    clean up

--- BookstoreAPI/Program.cs
+++ BookstoreAPI/Program.cs
@@ -7,11 +7,6 @@ using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<BookstoreDBContext>(options => options.UseSqlServer(
                    builder.Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Scoped);
 
@@ -41,8 +36,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
